72cd00a0ff913dcf5a1cbed2323b628480357172
Krutika Bhadane <krutikabhadane79@gmail.com>
2024-09-09 12:31:01 +0530

    Added: UC-1 Check Employee is Present or Absent

--- src/main/java/com/employeewage/EmployeeWageComputation.java
+++ src/main/java/com/employeewage/EmployeeWageComputation.java
@@ -5,5 +5,15 @@ public class EmployeeWageComputation
     public static void main( String[] args )
     {
         System.out.println( "Welcome to Employee Wage Computation Program" );
+        final int FULL_TIME = 1;
+        double empType = Math.floor(Math.random() * 10) % 2;
+        if (empType == FULL_TIME)
+        {
+            System.out.println("Employee is Present");
+        }
+        else
+        {
+            System.out.println("Employee is Absent");
+        }
     }
 }
