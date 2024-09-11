96171eefada9942ec45dce7a8049a2e835280d0b
Krutika Bhadane <krutikabhadane79@gmail.com>
2024-09-11 11:38:00 +0530

    Added: Uc8 Compute employee wage for multiple companies

--- src/main/java/com/employeewage/EmployeeWageComputation.java
+++ src/main/java/com/employeewage/EmployeeWageComputation.java
@@ -2,20 +2,20 @@ package com.employeewage;
 
 public class EmployeeWageComputation
 {
-    public static void calculateTotalWage()
+    public static void calculateTotalWage(String company, int WAGE_PER_HR, int MAX_WORKING_DAYS, int MAX_WORKING_HRS)
     {
-        System.out.println( "Welcome to Employee Wage Computation Program" );
         final int FULL_TIME = 2;
         final int PART_TIME = 1;
-        final int WAGE_PER_HR = 20;
-        final int MAX_WORKING_DAYS = 20;
-        final int MAX_WORKING_HRS = 100;
         int totalWage = 0;
         int workingHours = 0;
+        System.out.println("Details of "+company+ " employee");
+        System.out.println("Wage per hour "+WAGE_PER_HR);
+        System.out.println("Maximum working hours "+MAX_WORKING_HRS);
+        System.out.println("Maximum working days "+MAX_WORKING_DAYS);
         System.out.printf("%5s     %5s     %5s     %5s\n", "Day", "Workinghrs", "Wage", "Total working hrs");
 
         for (int day = 1,totalWorkingHrs = 0; day <= MAX_WORKING_DAYS
-                && totalWorkingHrs < MAX_WORKING_HRS; day++, totalWorkingHrs += workingHours) {
+                && totalWorkingHrs <= MAX_WORKING_HRS; day++, totalWorkingHrs += workingHours) {
 
             int empType = (int) (Math.random() * 10) % 3;
 
@@ -34,10 +34,15 @@ public class EmployeeWageComputation
             totalWage += wage;
             System.out.printf("%5d       %5d      %5d      %5d\n", day, workingHours, wage, totalWorkingHrs + workingHours);
         }
-        System.out.println("Total wage for a month is " + totalWage);
+        System.out.println("Total wage for a month of "+ company+ " employee is " + totalWage);
     }
     public static void main( String[] args )
     {
-        calculateTotalWage();
+        System.out.println( "Welcome to Employee Wage Computation Program" );
+        calculateTotalWage("Amazon", 20,16, 150);
+        calculateTotalWage("Flipkart",40,20,100);
+        calculateTotalWage("Nykaa",30,15,100);
+        calculateTotalWage("Big Bazaar",20,20,200);
+        calculateTotalWage("Dmart", 25,25, 150);
     }
 }
